d777864460d5be7034e872121cfae3293954a393
hereisethanhunt <vishal.bisht13104751@gmail.com>
2020-02-04 03:11:39 +0530

    +addition of redirection from root path to book library

--- src/container/BookLibrary/routes.js
+++ src/container/BookLibrary/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Redirect } from "react-router-dom";
 
 import BookLibraryEdit from "../../component/BookLibraryEdit";
 import BookLibraryView from "../../component/BookLibraryView";
@@ -8,6 +8,7 @@ import BookLibrary from "./bookLibrary";
 
 const bookLibraryRoutes = () => (
   <BrowserRouter>
+    <Route exact path="/" render={() => <Redirect to="/book-library" />} />
     <Route exact path="/book-library" component={BookLibrary} />
     <Route exact path="/book-library/add" component={BookLibraryAdd} />
     <Route exact path="/book-library/edit" component={BookLibraryEdit} />
